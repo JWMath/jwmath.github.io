87d12070824cd7d89bd4db44ddaa42824c84434d
JWMath <wjun.math@gmail.com>
2020-05-04 09:43:28 +0800

    1

--- js/translate.js
+++ js/translate.js
@@ -14,3 +14,4 @@ var b=s+h;c._pah=h;c._pas=s;c._pbi=b+'/translate_static/img/te_bk.gif';
 c._pci=b+'/translate_static/img/te_ctrl3.gif';c._pli=b+'/translate_static/img/loading.gif';
 c._plla=h+'/translate_a/l';c._pmi=b+'/translate_static/img/mini_google.png';c._ps=b+'/translate_static/css/translateelement.css';
 c._puh='translate.google.com';_loadCss(c._ps);_loadJs(b+'/translate_static/js/element/main.js');})();})();
+
